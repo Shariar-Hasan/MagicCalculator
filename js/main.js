ea04ba76a2bafa01a3c306b6c51b9f88807d5613
ethico <shahriar.hasan.1523@gmail.com>
2021-01-30 01:31:04 +0600

    multiple dot oparator fixed

--- js/main.js
+++ js/main.js
@@ -1,17 +1,35 @@
 
 const display = document.getElementById('output');
+let dotTracker = true;
+let calculated = false;
 
 function displayShow(value) {
-    const oparatorList = ['*','/','+','-','.'];
+    const oparatorList = ['*', '/', '+', '-', '.'];
     let displayValue = display.value;
-    if(!oparatorList.includes(displayValue[displayValue.length-1]) || !oparatorList.includes(value))
-    {
+
+    if ((!oparatorList.includes(displayValue[displayValue.length - 1]) || !oparatorList.includes(value))) {
+        if(calculated){
+            clearAll();
+            calculated = false;
+        }
         display.value = display.value + value;
+        if (value == '.') {
+            if (!dotTracker) {
+                deleteLastElement();
+            }
+            else{
+                dotTracker = false;
+            }
+        }
+        else if (value == '*' || value == '-' || value == '+' || value == '/') {
+            dotTracker = true;
+        }
     }
 }
 
 function clearAll() {
     display.value = '';
+    dotTracker = true;
 }
 
 function deleteLastElement() {
@@ -22,4 +40,5 @@ function deleteLastElement() {
 
 function calculate(value) {
     display.value = eval(value);
+    calculated = true;
 }
